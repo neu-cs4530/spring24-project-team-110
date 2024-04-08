86433efbf6beb1ea2c84eb64d7df717b35fac3ec
Daniella Calle <daniella.calle@gmail.com>
2024-04-08 12:02:18 -0400

    remove email verified var

--- frontend/src/components/Login/TownSelection.tsx
+++ frontend/src/components/Login/TownSelection.tsx
@@ -238,135 +238,129 @@ export default function TownSelection(): JSX.Element {
 
   return (
     <>
-      {(emailVerified && (
-        <form>
-          <Stack>
-            <Box p='4' borderWidth='1px' borderRadius='lg'>
-              <Heading as='h2' size='lg'>
-                Select a username
-              </Heading>
-
-              <FormControl>
-                <FormLabel htmlFor='name'>Name</FormLabel>
-                <Input
-                  autoFocus
-                  name='name'
-                  placeholder='Your name'
-                  value={userName}
-                  onChange={event => setUserName(event.target.value)}
-                  disabled={true}
-                />
-              </FormControl>
-            </Box>
-            <Box borderWidth='1px' borderRadius='lg'>
-              <Heading p='4' as='h2' size='lg'>
-                Create a New Town
-              </Heading>
-              <Flex p='4'>
-                <Box flex='1'>
-                  <FormControl>
-                    <FormLabel htmlFor='townName'>New Town Name</FormLabel>
-                    <Input
-                      name='townName'
-                      placeholder='New Town Name'
-                      value={newTownName}
-                      onChange={event => setNewTownName(event.target.value)}
-                    />
-                  </FormControl>
-                </Box>
-                <Box>
-                  <FormControl>
-                    <FormLabel htmlFor='isPublic'>Publicly Listed</FormLabel>
-                    <Checkbox
-                      id='isPublic'
-                      name='isPublic'
-                      isChecked={newTownIsPublic}
-                      onChange={e => {
-                        setNewTownIsPublic(e.target.checked);
-                      }}
-                    />
-                  </FormControl>
-                </Box>
-                <Box>
-                  <Button
-                    data-testid='newTownButton'
-                    onClick={handleCreate}
-                    isLoading={isJoining}
-                    disabled={isJoining}>
-                    Create
-                  </Button>
-                </Box>
-              </Flex>
-            </Box>
-            <Heading p='4' as='h2' size='lg'>
-              -or-
+      <form>
+        <Stack>
+          <Box p='4' borderWidth='1px' borderRadius='lg'>
+            <Heading as='h2' size='lg'>
+              Select a username
             </Heading>
 
-            <Box borderWidth='1px' borderRadius='lg'>
-              <Heading p='4' as='h2' size='lg'>
-                Join an Existing Town
-              </Heading>
-              <Box borderWidth='1px' borderRadius='lg'>
-                <Flex p='4'>
-                  <FormControl>
-                    <FormLabel htmlFor='townIDToJoin'>Town ID</FormLabel>
-                    <Input
-                      name='townIDToJoin'
-                      placeholder='ID of town to join, or select from list'
-                      value={townIDToJoin}
-                      onChange={event => setTownIDToJoin(event.target.value)}
-                    />
-                  </FormControl>
-                  <Button
-                    data-testid='joinTownByIDButton'
-                    onClick={() => handleJoin(townIDToJoin)}
-                    isLoading={isJoining}
-                    disabled={isJoining}>
-                    Connect
-                  </Button>
-                </Flex>
+            <FormControl>
+              <FormLabel htmlFor='name'>Name</FormLabel>
+              <Input
+                autoFocus
+                name='name'
+                placeholder='Your name'
+                value={userName}
+                onChange={event => setUserName(event.target.value)}
+                disabled={true}
+              />
+            </FormControl>
+          </Box>
+          <Box borderWidth='1px' borderRadius='lg'>
+            <Heading p='4' as='h2' size='lg'>
+              Create a New Town
+            </Heading>
+            <Flex p='4'>
+              <Box flex='1'>
+                <FormControl>
+                  <FormLabel htmlFor='townName'>New Town Name</FormLabel>
+                  <Input
+                    name='townName'
+                    placeholder='New Town Name'
+                    value={newTownName}
+                    onChange={event => setNewTownName(event.target.value)}
+                  />
+                </FormControl>
               </Box>
+              <Box>
+                <FormControl>
+                  <FormLabel htmlFor='isPublic'>Publicly Listed</FormLabel>
+                  <Checkbox
+                    id='isPublic'
+                    name='isPublic'
+                    isChecked={newTownIsPublic}
+                    onChange={e => {
+                      setNewTownIsPublic(e.target.checked);
+                    }}
+                  />
+                </FormControl>
+              </Box>
+              <Box>
+                <Button
+                  data-testid='newTownButton'
+                  onClick={handleCreate}
+                  isLoading={isJoining}
+                  disabled={isJoining}>
+                  Create
+                </Button>
+              </Box>
+            </Flex>
+          </Box>
+          <Heading p='4' as='h2' size='lg'>
+            -or-
+          </Heading>
 
-              <Heading p='4' as='h4' size='md'>
-                Select a public town to join
-              </Heading>
-              <Box maxH='500px' overflowY='scroll'>
-                <Table>
-                  <TableCaption placement='bottom'>Publicly Listed Towns</TableCaption>
-                  <Thead>
-                    <Tr>
-                      <Th>Town Name</Th>
-                      <Th>Town ID</Th>
-                      <Th>Activity</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {currentPublicTowns?.map(town => (
-                      <Tr key={town.townID}>
-                        <Td role='cell'>{town.friendlyName}</Td>
-                        <Td role='cell'>{town.townID}</Td>
-                        <Td role='cell'>
-                          {town.currentOccupancy}/{town.maximumOccupancy}
-                          <Button
-                            onClick={() => handleJoin(town.townID)}
-                            disabled={town.currentOccupancy >= town.maximumOccupancy || isJoining}
-                            isLoading={isJoining}>
-                            Connect
-                          </Button>
-                        </Td>
-                      </Tr>
-                    ))}
-                  </Tbody>
-                </Table>
-              </Box>
+          <Box borderWidth='1px' borderRadius='lg'>
+            <Heading p='4' as='h2' size='lg'>
+              Join an Existing Town
+            </Heading>
+            <Box borderWidth='1px' borderRadius='lg'>
+              <Flex p='4'>
+                <FormControl>
+                  <FormLabel htmlFor='townIDToJoin'>Town ID</FormLabel>
+                  <Input
+                    name='townIDToJoin'
+                    placeholder='ID of town to join, or select from list'
+                    value={townIDToJoin}
+                    onChange={event => setTownIDToJoin(event.target.value)}
+                  />
+                </FormControl>
+                <Button
+                  data-testid='joinTownByIDButton'
+                  onClick={() => handleJoin(townIDToJoin)}
+                  isLoading={isJoining}
+                  disabled={isJoining}>
+                  Connect
+                </Button>
+              </Flex>
             </Box>
-          </Stack>
-        </form>
-      )) || (
-        <Box>
-          <Heading>Verify email to join towns</Heading>
-        </Box>
-      )}
+
+            <Heading p='4' as='h4' size='md'>
+              Select a public town to join
+            </Heading>
+            <Box maxH='500px' overflowY='scroll'>
+              <Table>
+                <TableCaption placement='bottom'>Publicly Listed Towns</TableCaption>
+                <Thead>
+                  <Tr>
+                    <Th>Town Name</Th>
+                    <Th>Town ID</Th>
+                    <Th>Activity</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {currentPublicTowns?.map(town => (
+                    <Tr key={town.townID}>
+                      <Td role='cell'>{town.friendlyName}</Td>
+                      <Td role='cell'>{town.townID}</Td>
+                      <Td role='cell'>
+                        {town.currentOccupancy}/{town.maximumOccupancy}
+                        <Button
+                          onClick={() => handleJoin(town.townID)}
+                          disabled={town.currentOccupancy >= town.maximumOccupancy || isJoining}
+                          isLoading={isJoining}>
+                          Connect
+                        </Button>
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </Box>
+          </Box>
+        </Stack>
+      </form>
     </>
   );
 }
